3676ff591652033e2211939cc76a70111b6f19ca
Sanjeev <sanjeev.sritharan007@gmail.com>
2024-01-18 09:34:08 -0500

    sepia with cuda works but with weird artifacting

--- app.py
+++ app.py
@@ -85,22 +85,42 @@ class ImageDisplayApp(QMainWindow):
             
             # 44.3x increase in speed using CUDA
 
+    def rgbToSep(self, image):
+        sepia_filter = cp.array([[0.393, 0.769, 0.189],
+                             [0.349, 0.686, 0.168],
+                             [0.272, 0.534, 0.131]])
+        image_array_gpu = cp.asarray(image)
+        sepia_result_gpu = cp.dot(image_array_gpu.reshape(-1, 3), sepia_filter.T).reshape(image.shape)
+
+        # Clip the values to be in the valid range [0, 255]
+        sepia_result_gpu = cp.clip(sepia_result_gpu, 0, 255).astype(cp.uint8)
+        
+        # Step 3: Convert NumPy array back to QImage
+        sepia_result_cpu = cp.asnumpy(sepia_result_gpu)
+
+        return sepia_result_cpu
+
     def applySepia(self):
         if self.pixmap:
-            image = self.pixmap.toImage()
-            image = image.convertToFormat(QImage.Format.Format_RGBA8888)
+            image = self.pixmap.toImage().convertToFormat(QImage.Format.Format_RGB32)
             width = image.width()
             height = image.height()
-
-            for x in range(width):
-                for y in range(height):
-                    color = QColor(image.pixel(x, y))
-                    sr = min(int(0.393 * color.red() + 0.769 * color.green() + 0.189 * color.blue()), 255)
-                    sg = min(int(0.349 * color.red() + 0.686 * color.green() + 0.168 * color.blue()), 255)
-                    sb = min(int(0.272 * color.red() + 0.534 * color.green() + 0.131 * color.blue()), 255)
-                    sepia_color = QColor(sr, sg, sb, color.alpha())
-                    image.setPixelColor(x, y, sepia_color)
-            self.image_label.setPixmap(QPixmap.fromImage(image).scaled(self.image_label.size(), Qt.AspectRatioMode.KeepAspectRatio, Qt.TransformationMode.SmoothTransformation))
+            ptr = image.constBits()
+            ptr.setsize(height * width * 4)
+            arr = np.frombuffer(ptr, np.uint8).reshape((height, width, 4))
+            img_gpu = cp.array(arr, dtype=cp.float32)
+            sepia_array = self.rgbToSep(img_gpu)
+            sepia_image = QImage(sepia_array.data, width, height, sepia_array.strides[0], QImage.Format.Format_RGB32)
+            # for x in range(width):
+            #     for y in range(height):
+            #         color = QColor(image.pixel(x, y))
+            #         sr = min(int(0.393 * color.red() + 0.769 * color.green() + 0.189 * color.blue()), 255)
+            #         sg = min(int(0.349 * color.red() + 0.686 * color.green() + 0.168 * color.blue()), 255)
+            #         sb = min(int(0.272 * color.red() + 0.534 * color.green() + 0.131 * color.blue()), 255)
+            #         sepia_color = QColor(sr, sg, sb, color.alpha())
+            #         image.setPixelColor(x, y, sepia_color)
+            
+            self.image_label.setPixmap(QPixmap.fromImage(sepia_image).scaled(self.image_label.size(), Qt.AspectRatioMode.KeepAspectRatio, Qt.TransformationMode.SmoothTransformation))
             self.image_label.adjustSize()
 
 if __name__ == '__main__':
