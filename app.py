fb0f09d74252d1acd05b5d4fc1ae444172c66069
Sanjeev <sanjeev.sritharan007@gmail.com>
2024-01-18 01:14:20 -0500

    Basic UI and grayscale with cuda implemented

---
+++ app.py
@@ -0,0 +1,77 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QFileDialog, QVBoxLayout, QPushButton, QWidget
+from PyQt6.QtGui import QPixmap, QImage, QColor
+from PyQt6.QtCore import Qt
+import numpy as np
+import cupy as cp
+
+class ImageDisplayApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        # Create central widget and set layout
+        central_widget = QWidget(self)
+        self.setCentralWidget(central_widget)
+        layout = QVBoxLayout(central_widget)
+
+        # Create QLabel to display the image
+        self.image_label = QLabel(self)
+        layout.addWidget(self.image_label)
+
+        # Create 'Open Image' button
+        open_button = QPushButton('Open Image', self)
+        open_button.clicked.connect(self.openImage)
+        layout.addWidget(open_button)
+
+        # Create 'Grayscale' button
+        grayscale_button = QPushButton('Grayscale', self)
+        grayscale_button.clicked.connect(self.applyGrayscale)
+        layout.addWidget(grayscale_button)
+
+        # Set up the main window
+        self.setGeometry(100, 100, 800, 600)
+        self.setWindowTitle('Image Display App')
+
+        self.pixmap = None
+
+    def openImage(self):
+        fileName, _ = QFileDialog.getOpenFileName(self, "Open Image File", "", "Image Files (*.png *.jpg *.bmp *.gif *.jpeg)")
+        
+        if fileName:
+            self.pixmap = QPixmap(fileName)
+            self.image_label.setPixmap(self.pixmap.scaled(self.image_label.size(), Qt.AspectRatioMode.KeepAspectRatio, Qt.TransformationMode.SmoothTransformation))
+            self.image_label.adjustSize()
+
+    def rgbToGray(self, image):
+        gray_gpu = cp.dot(image[..., :3], cp.asarray([0.299, 0.587, 0.114]))
+        # Normalize and cast to uint8
+        gray_gpu = cp.clip(gray_gpu, 0, 255).astype(cp.uint8)
+        gray_cpu = cp.asnumpy(gray_gpu)
+        
+        return gray_cpu
+
+    def applyGrayscale(self):
+        if self.pixmap:
+            image = self.pixmap.toImage().convertToFormat(QImage.Format.Format_RGBA8888)
+            width = image.width()
+            height = image.height()
+            ptr = image.constBits()
+            ptr.setsize(height * width * 4)
+            arr = np.frombuffer(ptr, np.uint8).reshape((height, width, 4))
+            img_gpu = cp.array(arr, dtype=cp.float32)
+            gray_array = self.rgbToGray(img_gpu)
+            print(gray_array.data)
+            gray_image = QImage(gray_array.data, width, height, gray_array.strides[0], QImage.Format.Format_Grayscale8)
+
+            self.image_label.setPixmap(QPixmap.fromImage(gray_image).scaled(self.image_label.size(), Qt.AspectRatioMode.KeepAspectRatio, Qt.TransformationMode.SmoothTransformation))
+            self.image_label.adjustSize()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mainWindow = ImageDisplayApp()
+    mainWindow.show()
+    sys.exit(app.exec())
---
+++ cuda.py
@@ -0,0 +1,5 @@
+import cupy as cp
+import numpy as np
+
+array_cpu = np.random.randint(0, 255, size=(2000, 2000))
+print(array_cpu)
